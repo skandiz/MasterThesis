7abcef2f768a507726fa06e31c4ed5c7a3899ab0
skandiz <scandixd@gmail.com>
2023-03-07 22:21:18 +0100

    cose

--- code/udf.py
+++ code/udf.py
@@ -5,3 +5,18 @@ def powerLawFit(funct, fit, powerlawExponents):
         powerlawExponents[i] = powerlawFit.n.values 
         fit[i] = powerlawFit.A.values * np.array(funct.index)**powerlawExponents[i] 
     return fit, powerlawExponents
+
+# Histogram fit
+def fit_hist(y, bins_, distribution):
+    if distribution == normal_distr:
+        p0_ = [1., 0.]
+    else:
+        p0_ = [1.]
+
+    bins_c = bins_[:-1] + np.diff(bins_) / 2
+
+    bin_heights, _ = np.histogram(y, bins = bins_, density = True)
+    ret, pcov = curve_fit(distribution, bins_c , bin_heights, p0 = p0_)
+    ret_std = np.sqrt(np.diag(pcov))
+    
+    return ret, ret_std
