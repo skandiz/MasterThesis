555f33cdaf5cf88010e0868b1534c0deede0dbe4
skandiz <scandixd@gmail.com>
2023-03-10 14:51:54 +0100

    creation of utiluty.py to clean up the code

--- code/udf.py
+++ code/udf.py
@@ -2,35 +2,73 @@ import numpy as np
 import trackpy as tp
 from scipy.optimize import curve_fit
 from yupi import Trajectory
+import yupi.stats as ys
+from tqdm import tqdm
 
 # get trajectories
-def get_trajectories(nDrops, red_particle_idx, rawTrajs, smoothTrajs):
+def get_trajs(nDrops, red_particle_idx, trajs):
     # raw trajectories
     blueTrajs = []
     redTraj = []
     for i in range(0, nDrops):
         if i == red_particle_idx:
-            p = rawTrajs.loc[rawTrajs.particle == i, ["x","y"]]
+            p = trajs.loc[trajs.particle == i, ["x","y"]]
             redTraj.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
         else:
-            p = rawTrajs.loc[rawTrajs.particle == i, ["x","y"]]
+            p = trajs.loc[trajs.particle == i, ["x","y"]]
             blueTrajs.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
-        
-    # smooth trajectories
-    blueTrajs_smooth = []
-    redTraj_smooth = []
-    for i in range(0, nDrops):
-        if i == red_particle_idx:
-            p = smoothTrajs.loc[smoothTrajs.particle == i, ["x","y"]]
-            redTraj_smooth.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
-        else:
-            p = smoothTrajs.loc[smoothTrajs.particle == i, ["x","y"]]
-            blueTrajs_smooth.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
+
+    return blueTrajs, redTraj
+
+# get speed distributions windowed in time
+def speed_windowed(nDrops, nSteps, startFrames, endFrames, red_particle_idx, trajs):
+    v_blue_wind = []
+    v_red_wind = []
+    for k in tqdm(range(nSteps)):
+        trajs_wind = trajs.loc[trajs.frame.between(startFrames[k], endFrames[k])]
+        blueTrajs = []
+        redTraj = []
+
+        for i in range(0, nDrops):
+            if i == red_particle_idx:
+                p = trajs_wind.loc[trajs_wind.particle==i, ["x","y"]]
+                redTraj.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
+            else:
+                p = trajs_wind.loc[trajs_wind.particle==i, ["x","y"]]
+                blueTrajs.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
             
-    return blueTrajs, redTraj, blueTrajs_smooth, redTraj_smooth
+        v_blue_wind.append(ys.speed_ensemble(blueTrajs, step=10))
+        v_red_wind.append(ys.speed_ensemble(redTraj, step=10))
+    
+    return v_blue_wind, v_red_wind
+    
+# get turning angles distributions windowed in time
+def theta_windowed(nDrops, nSteps, startFrames, endFrames, red_particle_idx, trajs):
+    theta_blue_wind = []
+    theta_red_wind = []
+    for k in tqdm(range(nSteps)):
+        trajs_wind = trajs.loc[trajs.frame.between(startFrames[k], endFrames[k])]
+        blueTrajs = []
+        redTraj = []
+
+        for i in range(0, nDrops):
+            if i == red_particle_idx:
+                p = trajs_wind.loc[trajs_wind.particle==i, ["x","y"]]
+                redTraj.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
+            else:
+                p = trajs_wind.loc[trajs_wind.particle==i, ["x","y"]]
+                blueTrajs.append(Trajectory(p.x, p.y, dt = 1/10, traj_id=i))
+
+        theta_blue_wind.append(ys.turning_angles_ensemble(blueTrajs, centered= True))
+        theta_red_wind.append(ys.turning_angles_ensemble(redTraj, centered= True))
+        
+    return theta_blue_wind, theta_red_wind
+
 
 # Power Law fit
-def powerLawFit(funct, fit, powerlawExponents, nDrops):
+def powerLawFit(funct, nDrops):
+    fit = np.zeros(funct.shape).T
+    powerlawExponents = np.zeros(nDrops)
     for i in range(nDrops):
         powerlawFit = tp.utils.fit_powerlaw(funct[i], plot = False) 
         powerlawExponents[i] = powerlawFit.n.values 
